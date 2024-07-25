7a1fb8a87eb07c989ca5910c7c578b050c2a7280
rama69 <ramatoulaye.sou@gmail.com>
2024-07-25 11:03:01 +0200

    custom validators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+
+
+export const motsInterdits = (control:AbstractControl): ValidationErrors | null=> {
+   const mots = ["lol", "mdr"];
+   return mots.includes(control.value) ? {interdit:"Ce mot est interdit"}:null
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -20,14 +26,18 @@ export class HomeComponent {
   }*/
 
     public form:FormGroup = new FormGroup({
-      name: new FormControl('', {validators:[Validators.required, Validators.minLength(4), Validators.max(10)]}),
-      email: new FormControl('', {validators:[Validators.email]}),
+      name: new FormControl('', {validators:[Validators.required, Validators.minLength(4), Validators.maxLength(10), motsInterdits]}),
+      email: new FormControl('', {validators:[Validators.email, motsInterdits]}),
 
     })
 
     onSubmit(){
-      console.log(this.form.get('name')?.value)
-      console.log(this.form.get('email')?.value)
+      if(this.form.valid){
+        console.log(this.form.get('name')?.value);
+        console.log(this.form.get('email')?.value);
+        this.form.reset();
+      }
+     
     }
 
 }
